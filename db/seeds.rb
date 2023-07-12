c8c93329863c8e421731dbca0dd4ac2826297f96
MICHAELKITH <michaelkithinji27@gmail.com>
2023-07-12 17:23:35 +0300

    checking linters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
 greetings = [
-  "Hello World!",
-  "Hello Universe!",
-  "Hello Cosmos!",
-  "Hi there!",
-  "Howdy!"
+  'Hello World!',
+  'Hello Universe!',
+  'Hello Cosmos!',
+  'Hi there!',
+  'Howdy!'
 ]
 
-
 greetings.each do |message|
-  Greeting.create(message: message)
+  Greeting.create(message:)
 end
